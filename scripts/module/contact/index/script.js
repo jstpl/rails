a54b7cf483a69784311e1d06e6e5f84957e51fc1
Vitaliy <theyamshikov@yandex.ru>
2019-10-11 18:03:09 +0600

    update

---
+++ scripts/module/contact/index/script.js
@@ -0,0 +1,14 @@
+//$(function() {
+
+namespace.define('bundle.contact.controller');
+
+    bundle.contact.controller.indexController = {
+        indexAction: function (params) {
+            alert('indexAction');
+        },
+        init: function (params) {
+            alert('init');
+        }
+    };
+
+//});
