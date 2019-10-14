891d009a906e5f896affc8b8100b730bf588d571
Vitaliy <theyamshikov@yandex.ru>
2019-10-14 23:10:28 +0600

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,82 +3,98 @@ var concat = require('gulp-concat');
 const minify = require('gulp-minify');
 var concatCss = require('gulp-concat-css');
 
+var styleSrc = [
+    './vendor/bootstrap/3.3.7/bootstrap.min.css',
+    './vendor/bootstrap/3.3.7/bootstrap-theme.min.css',
+    './vendor/toastr/toastr-2.1.4.min.css',
+];
+
+var vendorSrc = [
+    './vendor/jquery/jquery-2.2.4.min.js',
+    './vendor/jquery/jquery-ui-1.12.1.min.js',
+    './vendor/lodash/4.17.15/lodash.js',
+    './vendor/bootstrap/3.3.7/bootstrap.min.js',
+    './vendor/director/director-1.2.6.min.js',
+    './vendor/redux/redux-3.5.2.min.js',
+    './vendor/vue/vue-2.6.10.js',
+    './vendor/toastr/toastr-2.1.4.min.js',
+];
+
+
+var bundleSrc = [
+    './bundle/kernel/namespace.js',
+    './bundle/helper/*.js',
+    './bundle/env/*.js',
+    './bundle/log/*.js',
+    './bundle/notify/*.js',
+    './bundle/queue/*.js',
+    './bundle/ui/*.js',
+    './bundle/kernel/container.js',
+    './bundle/kernel/bootstrap.js',
+    './bundle/kernel/func.js',
+    './bundle/domain/*.js',
+    './bundle/cache/*.js',
+    './bundle/event/*.js',
+    './bundle/rest/*.js',
+    './bundle/legalbet/*.js',
+    './bundle/widget/*.js',
+    './bundle/spa/*.js',
+    './bundle/vue/*.js',
+];
+
+var appSrc = [
+    './module/**/config/*.js',
+    './module/app/bootstrap.js',
+    './module/navbar/**/*.js',
+
+    './module/user/store/*.js',
+    './module/user/service/*.js',
+    './module/user/lang/ru/*.js',
+
+    './module/contact/store/*.js',
+    './module/contact/service/*.js',
+
+    './module/notify/enum/*.js',
+    './module/notify/service/*.js',
+];
+
+var allSrc = vendorSrc.concat(bundleSrc).concat(appSrc);
+
 gulp.task('build', function() {
 
-    gulp.src([
-        './vendor/bootstrap/3.3.7/bootstrap.min.css',
-        './vendor/bootstrap/3.3.7/bootstrap-theme.min.css',
-        './vendor/toastr/toastr-2.1.4.min.css',
-    ])
+    gulp.src(styleSrc)
         .pipe(concatCss("app.css"))
         .pipe(gulp.dest('./dist/style/'));
 
-    gulp.src([
-        './vendor/jquery/jquery-2.2.4.min.js',
-        './vendor/jquery/jquery-ui-1.12.1.min.js',
-        './vendor/lodash/4.17.15/lodash.js',
-        './vendor/bootstrap/3.3.7/bootstrap.min.js',
-        './vendor/director/director-1.2.6.min.js',
-            './vendor/redux/redux-3.5.2.min.js',
-            './vendor/vue/vue-2.6.10.js',
-            './vendor/toastr/toastr-2.1.4.min.js',
-            //'./vendor/toastr/toastr-2.1.4.min.css',
+    gulp.src(allSrc, { sourcemaps: true })
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./dist/script/'));
 
-        //'./vendor/**/*.js'
-    ], { sourcemaps: true })
+    /*gulp.src(vendorSrc, { sourcemaps: true })
         .pipe(concat('vendor.js'))
-        //.pipe(minify())
-        .pipe(gulp.dest('./dist/js/'));
+        .pipe(gulp.dest('./dist/script/'));
 
-    gulp.src([
-        './bundle/kernel/namespace.js',
-        './bundle/helper/*.js',
-        './bundle/env/*.js',
-        './bundle/log/*.js',
-        './bundle/notify/*.js',
-        './bundle/queue/*.js',
-        './bundle/ui/*.js',
-        './bundle/kernel/container.js',
-        './bundle/kernel/bootstrap.js',
-        './bundle/kernel/func.js',
-        './bundle/domain/*.js',
-        './bundle/cache/*.js',
-        './bundle/event/*.js',
-        './bundle/rest/*.js',
-        './bundle/legalbet/*.js',
-        './bundle/widget/*.js',
-        './bundle/spa/*.js',
-        './bundle/vue/*.js',
-
-        //'./bundle/**/*.js',
-    ], { sourcemaps: true })
+    gulp.src(bundleSrc, { sourcemaps: true })
         .pipe(concat('bundle.js'))
-        //.pipe(minify())
-        .pipe(gulp.dest('./dist/js/'));
+        .pipe(gulp.dest('./dist/script/'));
 
-    gulp.src([
-        './module/**/config/*.js',
-        './module/app/bootstrap.js',
-        './module/navbar/**/*.js',
-
-        //'./module/user/config/*.js',
-        './module/user/store/*.js',
-        './module/user/service/*.js',
-        './module/user/lang/ru/*.js',
-
-        //'./module/contact/config/*.js',
-        './module/contact/store/*.js',
-        './module/contact/service/*.js',
-
-        './module/notify/enum/*.js',
-        './module/notify/service/*.js',
-
-
-
-    ], { sourcemaps: true })
+    gulp.src(appSrc, { sourcemaps: true })
         .pipe(concat('app.js'))
-        //.pipe(minify())
-        .pipe(gulp.dest('./dist/js/'));
+        .pipe(gulp.dest('./dist/script/'));*/
+
+});
+
+gulp.task('minify', function() {
+
+    gulp.src('./dist/script/all.js')
+        .pipe(minify())
+        .pipe(gulp.dest('./dist/script/'));
+
+    /*gulp.src([
+        './dist/style/app.css',
+    ])
+        .pipe(minify())
+        .pipe(gulp.dest('./dist/style/'));*/
 
 });
 
