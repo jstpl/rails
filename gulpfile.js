537aab820d337c859529732e90a5cac78e6e481f
vitaliy <theyamshikov@yandex.ru>
2019-10-16 10:54:33 +0600

    555

--- gulpfile.js
+++ gulpfile.js
@@ -93,48 +93,68 @@ var appSrc = [
 
 var allSrc = vendorSrc.concat(bundleSrc).concat(appSrc);
 
-gulp.task('build', function() {
+var build = {
+    firstCharExp: /^([\s\S]{1})/g,
+    main: function () {
+        gulp.src(styleSrc)
+            .pipe(concatCss(config.dev.styleFileName))
+            .pipe(gulp.dest(config.dist.styleOutputPath));
 
-    gulp.src(styleSrc)
-        .pipe(concatCss(config.dev.styleFileName))
-        .pipe(gulp.dest(config.dist.styleOutputPath));
+        gulp.src(allSrc, { sourcemaps: true })
+            .pipe(concat(config.dev.scriptFileName))
+            .pipe(gulp.dest(config.dist.scriptOutputPath));
+    },
+    min: function () {
+        gulp.src(config.dist.scriptOutputPath + config.dev.scriptFileName)
+            .pipe(minify())
+            .pipe(gulp.dest(config.min.scriptOutputPath));
 
-    gulp.src(allSrc, { sourcemaps: true })
-        .pipe(concat(config.dev.scriptFileName))
-        .pipe(gulp.dest(config.dist.scriptOutputPath));
+        gulp.src([
+            config.dist.styleOutputPath + config.dev.styleFileName,
+        ])
+            .pipe(minify())
+            .pipe(gulp.dest(config.dist.styleOutputPath));
+
+        gulp.src(config.dist.styleOutputPath + config.dev.styleFileName)
+            .pipe(csso())
+            .pipe(gulp.dest(config.min.styleOutputPath));
+    },
+    renderIncludedList: function (fileMap) {
+        var listFiles = '';
+        for(var k in fileMap) {
+            var url = fileMap[k];
+            var item = ''+url+'';
+            listFiles = listFiles + "\n" + item;
+        }
+        var listFilesDocBlock = '/**\nIncluded files:'+listFiles+'\n*/';
+        return listFilesDocBlock;
+    },
+    vendor: function () {
+        var listFilesDocBlock = build.renderIncludedList(vendorSrc);
+        gulp.src(vendorSrc, { sourcemaps: true })
+            .pipe(concat('vendor.js'))
+            .pipe(replace(build.firstCharExp, listFilesDocBlock + '\n\n$1'))
+            .pipe(gulp.dest(config.dev.scriptOutputPath));
+
+        var listFilesDocBlockStyle = build.renderIncludedList(styleSrc);
+        gulp.src(styleSrc)
+            .pipe(concatCss('vendor.css'))
+            .pipe(replace(build.firstCharExp,  listFilesDocBlockStyle + '\n\n$1'))
+            .pipe(gulp.dest(config.dev.styleOutputPath));
+    },
+};
+
+gulp.task('build', build.main);
+gulp.task('build-min', build.min);
+gulp.task('build-vendor', build.vendor);
+
+gulp.task('build-all', function() {
+
+    build.main();
+    build.min();
+    build.vendor();
 
 });
-
-gulp.task('build-vendor', function() {
-
-    gulp.src(vendorSrc, { sourcemaps: true })
-        .pipe(concat('vendor.js'))
-        .pipe(gulp.dest(config.dev.scriptOutputPath));
-
-    gulp.src(styleSrc)
-        .pipe(concatCss('vendor.css'))
-        .pipe(gulp.dest(config.dev.styleOutputPath));
-
-});
-
-gulp.task('build-min', function() {
-
-    gulp.src(config.dist.scriptOutputPath + config.dev.scriptFileName)
-        .pipe(minify())
-        .pipe(gulp.dest(config.min.scriptOutputPath));
-
-    gulp.src([
-        config.dist.styleOutputPath + config.dev.styleFileName,
-    ])
-        .pipe(minify())
-        .pipe(gulp.dest(config.dist.styleOutputPath));
-
-    gulp.src(config.dist.styleOutputPath + config.dev.styleFileName)
-        .pipe(csso())
-        .pipe(gulp.dest(config.min.styleOutputPath));
-
-});
-
 
 gulp.task('renderScripts', function() {
 
