d4e05f14ffe9a0df679a911a31f47785df22f70d
vitaliy <theyamshikov@yandex.ru>
2019-10-16 14:59:12 +0600

    4455

--- src/module/bskit/controller/allController.js
+++ src/module/bskit/controller/allController.js
@@ -9,12 +9,53 @@ $(function () {
                 id: 'button',
             },
             {
+                title: 'container',
+                id: 'container',
+            },
+            {
+                title: 'dialog',
+                id: 'dialog',
+            },
+            {
+                title: 'dropdown',
+                id: 'dropdown',
+            },
+            {
+                title: 'form',
+                id: 'form',
+            },
+            {
+                title: 'indicator',
+                id: 'indicator',
+            },
+            {
                 title: 'modal',
                 id: 'modal',
             },
             {
+                title: 'nav',
+                id: 'nav',
+            },
+            {
+                title: 'navbar',
+                id: 'navbar',
+            },
+            {
                 title: 'notification',
                 id: 'notification',
+            },
+            {
+                title: 'progressbar',
+                id: 'progressbar',
+            },
+            {
+                title: 'table',
+                id: 'table',
+            },
+
+            {
+                title: 'typography',
+                id: 'typography',
             },
         ],
     };
