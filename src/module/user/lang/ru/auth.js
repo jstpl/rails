a7ee34646b15ab44b4c8b0f04692d7a0d738d62f
vitaliy <theyamshikov@yandex.ru>
2019-10-25 13:59:14 +0600

    add lang

--- src/module/user/lang/ru/auth.js
+++ src/module/user/lang/ru/auth.js
@@ -2,6 +2,7 @@ space('lang.user.auth', function() {
 
     return {
 
+        failAuthorizationMessage: 'Неверный логин или пароль',
         successAuthorizationMessage: 'Вы авторизованы',
         successLogoutMessage: 'Вы вышли из аккаунта',
         authorizationRequiredMessage: 'Необходимо авторизоваться',
