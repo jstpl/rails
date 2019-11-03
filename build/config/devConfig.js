5f3923a3a6daf04d99f3b8174476025e83765d07
Vitaliy <theyamshikov@yandex.ru>
2019-11-03 14:07:29 +0600

    888

--- build/config/devConfig.js
+++ build/config/devConfig.js
@@ -1,68 +1,33 @@
-module.exports = {
-    baseUrl: ".",
-    out: "src/assets/vendor.js",
-    paths: {
-        jrails: 'node_modules/jrails/src',
-        DirectorRouter: 'node_modules/director/build/director.min',
-        lodash: 'node_modules/lodash/lodash.min',
-        jquery: 'node_modules/jquery/dist/jquery.min',
-        text: 'node_modules/text/text',
-        twitterBootstrap: 'node_modules/bootstrap/dist/js/bootstrap.min',
-        //redux: 'node_modules/redux/dist/redux.min',
-        vue: 'node_modules/vue/dist/vue.min',
-        //toastr: 'node_modules/toastr/build/toastr.min',
-        toastr: 'node_modules/jrails/src/notify/toastrMock',
-        jqueryUi: 'node_modules/jquery-ui/jquery-ui.min',
-    },
-    include: [
-        'node_modules/requirejs/require',
-        'DirectorRouter',
-        'lodash',
-        'jquery',
-        'twitterBootstrap',
-        'vue',
-        'text',
-        'jqueryUi',
-        'jrails/spa/router',
-        'jrails/helper/php',
-        'jrails/event/eventService',
-        'jrails/helper/class',
-        'jrails/helper/localStorage',
-        'jrails/domain/baseLocalStorage',
-        'jrails/kernel/container',
-        'jrails/rest/client',
-        'jrails/notify/notifyTypeEnum',
-        'jrails/notify/notifyService',
-        'jrails/notify/driver/toastrDriver',
-        'jrails/bootstrap/modal/modalService',
-        'jrails/vue/vm',
-        'jrails/spa/layer',
-        'jrails/spa/query',
-        'jrails/spa/controllerFactory',
-    ],
-    shim: {
-        'DirectorRouter': {
-            exports: 'Router'
-        },
-        'lodash': {
-            exports: '_'
-        },
-        'jquery': {
-            exports: '$'
-        },
-        "jqueryUi": {
-            exports: "$",
-            deps: ['jquery']
-        },
-        "twitterBootstrap": {
-            deps: ["jquery"]
-        },
-        'toastr': {
-            exports: 'toastr',
-            deps: ["jquery"]
-        },
-        'vue': {
-            exports: 'Vue'
-        },
-    }
-};
+
+// наследуем конфиг
+var config = require('jrails/requirejs/requirejs-deps');
+// указываем в какой файл собирать
+config.out = "src/assets/vendor.js";
+config.include = [
+    'node_modules/requirejs/require',
+    'DirectorRouter',
+    'lodash',
+    'jquery',
+    'twitterBootstrap',
+    'vue',
+    'text',
+    'jqueryUi',
+    'jrails/spa/router',
+    'jrails/helper/php',
+    'jrails/event/eventService',
+    'jrails/helper/class',
+    'jrails/helper/localStorage',
+    'jrails/domain/baseLocalStorage',
+    'jrails/kernel/container',
+    'jrails/rest/client',
+    'jrails/notify/notifyTypeEnum',
+    'jrails/notify/notifyService',
+    'jrails/notify/driver/toastrDriver',
+    'jrails/bootstrap/modal/modalService',
+    'jrails/vue/vm',
+    'jrails/spa/layer',
+    'jrails/spa/query',
+    'jrails/spa/controllerFactory',
+];
+
+module.exports = config;
