51aca3afd2fdc327fa87b372e7365c9b69252f2a
Vitaliy <theyamshikov@yandex.ru>
2019-11-03 13:30:46 +0600

    111

--- build/dist.js
+++ build/dist.js
@@ -1,6 +1,6 @@
 
 var requirejs = require('requirejs');
-var config = require('./distConfig');
+var config = require('./config/distConfig');
 
 requirejs.optimize( config, function(results) {
     console.log(results);
